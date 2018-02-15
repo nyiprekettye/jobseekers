6bd50734186ab5af7a5bae2b81bb64e6ac74cd18
nyiprekettye <nyiprekettye@gmail.com>
2018-02-15 15:08:03 +0100

    Initialization React and webpack.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var _ = require('underscore');
-var app = express();
-var session = require('express-session');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const _ = require('underscore');
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+//var React = require('react')
+//var ReactDOM = require('react-dom')
 
 app.set('port', process.env.PORT || 8080);
 app.set('jwtTokenSecret', '123456ABCDEF');
-app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(express.static(path.join(__dirname, './dist')));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(session({
@@ -19,10 +20,15 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.get('/', (req, res) => {
+    console.log('Serving ', req.url);
+    res.sendFile(__dirname + '/dist/app.html');
+});
+/*
 app.get('/', function (req, res) {
     res.send('Hello World')
-})
-
+});
+*/
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
-})
+});
